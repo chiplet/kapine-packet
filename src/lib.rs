9688521324614c2e8af55c8700afb2cd13f1f1ad
Ottis1 <otto.kuusela@outlook.com>
2021-08-03 19:33:54 +0300

    Make payload optional

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub struct Packet {
     pub sync: u16,
     pub command: u8,
     pub length: u8,
-    pub payload: [u8; 255],
+    pub payload: Option<[u8; 255]>,
     pub checksum: u16
 }
 
@@ -25,14 +25,19 @@ impl Packet {
         buffer[i] = self.length;
         i = i + 1;
 
-        let mut j = 0;
-        let mut byte = self.payload[j];
-        while byte != 0x00 {
-            buffer[i] = self.payload[j];
-            i = i + 1;
-            j = j + 1;
-            byte = self.payload[j]
-        }
+        match self.payload {
+            None => (),
+            Some(payload) => {
+                let mut j = 0;
+                let mut byte = payload[j];
+                while byte != 0x00 {
+                    buffer[i] = payload[j];
+                    i = i + 1;
+                    j = j + 1;
+                    byte = payload[j]
+                }
+            }
+        };
 
         buffer[i] = self.checksum as u8;
         i = i + 1;
@@ -48,28 +53,48 @@ impl Packet {
         state.update(&self.sync.to_le_bytes());
         state.update(&self.command.to_le_bytes());
         state.update(&self.length.to_le_bytes());
-        let payload_len = self.length as usize;
-        state.update(&self.payload[0..payload_len]);
+        match self.payload {
+            None => (),
+            Some(payload) => {
+                let payload_len = self.length as usize;
+                state.update(&payload[0..payload_len]);
+            }
+        };
 
         self.checksum = state.get()
     }
 
-    pub fn new(command: u8, payload: &[u8]) -> Self {
-        let payload_length = payload.len();
-        assert!(payload_length <= 255);
+    pub fn new(command: u8, payload: Option<&[u8]>) -> Self {
 
-        let mut payload_buf = [0u8; 255];
-        for (src, dest) in payload.iter().zip(payload_buf.iter_mut()) {
-            *dest = *src;
-        }
+        let mut packet = match payload {
+            None => {
+                Packet {
+                    sync: 0x55AA,
+                    command,
+                    length: 0u8,
+                    payload: None,
+                    checksum: 0u16, // populated by `compute_checksum`
+                }
+            },
+            Some(p) => {
+                let payload_length = p.len();
+                assert!(payload_length <= 255);
 
-        let mut packet = Packet {
-            sync: 0x55AA,
-            command: command,
-            length: payload_length as u8,
-            payload: payload_buf,
-            checksum: 0u16, // populated by `compute_checksum`
+                let mut payload_buf = [0u8; 255];
+                for (src, dest) in p.iter().zip(payload_buf.iter_mut()) {
+                    *dest = *src;
+                }
+
+                Packet {
+                    sync: 0x55AA,
+                    command,
+                    length: payload_length as u8,
+                    payload: Some(payload_buf),
+                    checksum: 0u16, // populated by `compute_checksum`
+                }
+            }
         };
+
         packet.compute_checksum();
         packet
     }
@@ -84,15 +109,21 @@ mod tests {
     fn new_is_correct() {
         let payload = b"Hello, world!";
         let command = 0x03;
-        let packet = Packet::new(command, &payload[..]);
+        let packet = Packet::new(command, Some(&payload[..]));
         assert_eq!(packet.command, command);
 
-        for i in 0..payload.len() {
-            assert_eq!(packet.payload[i], payload[i]);
+        match packet.payload {
+            None => (),
+            Some(p) => {
+                for i in 0..payload.len() {
+                    assert_eq!(p[i], payload[i]);
+                }
+                for i in payload.len()..255 {
+                    assert_eq!(p[i], 0);
+                }
+            }
         }
-        for i in payload.len()..255 {
-            assert_eq!(packet.payload[i], 0);
-        }
+
     }
 
     #[test]
@@ -100,7 +131,7 @@ mod tests {
     fn new_disallows_large_payloads() {
         let too_large = [3; 256];
         let command = 0x05;
-        let packet = Packet::new(command, &too_large[..]);
+        let packet = Packet::new(command, Some(&too_large[..]));
     }
 
     #[test]
@@ -110,36 +141,17 @@ mod tests {
 
         let command = 0x01;
         let payload = [0xAA, 0xAA, 0xAA];
-        let p = Packet::new(command, &payload[..]);
+        let p = Packet::new(command, Some(&payload[..]));
 
         assert_eq!(p.checksum, example_checksum);
     }
 
     #[test]
-    fn packet_test() {
-        const BUF_SIZE: usize = 261;
-        let mut buffer: [u8; BUF_SIZE] = [0; BUF_SIZE];
+    fn no_payload() {
+        let command = 0x08;
+        let p = Packet::new(command, None);
 
-        let packet: [u8; 7] = [0xAA, 0x55, 0x01, 0x03, 0x12, 0x34, 0x56];
-
-        let mut payload = [0; 255];
-        payload[0] = 1;
-        payload[1] = 2;
-        payload[2] = 3;
-        let packet_struct = Packet {
-            sync: 0x55AA,
-            command: 0x01,
-            length: 3,
-            payload: payload,
-            checksum: 0u16,
-        };
-
-        println!("{:?}\n", packet_struct);
-
-        let len = packet_struct.write_bytes(&mut buffer);
-
-        println!("bytes: {:?}", buffer);
-        println!("length: {:?}", len);
+        assert_eq!(p.length, 0);
     }
-}
 
+}
